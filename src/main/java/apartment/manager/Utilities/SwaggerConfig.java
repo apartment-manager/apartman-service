b409e3643326a405acb17333eb603ce971057573
Khaled Asfur <khalid_asfur@hotmail.com>
2024-10-16 01:39:12 +0300

    Add swagger configuration

---
+++ src/main/java/apartment/manager/Utilities/SwaggerConfig.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright © 2024 Khaled Asfour
+ */
+package apartment.manager.Utilities;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SwaggerConfig class is responsible for configuring and managing
+ * Swagger API documentation in the application. This configuration allows
+ * customization of API groups and the exclusion of certain paths from
+ * the API documentation.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * Defines a Swagger group for all APIs in the application, excluding
+     * certain paths (like disabled APIs) from being documented.
+     */
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("All APIs")                  // Defines the group name for Swagger UI
+                .pathsToExclude("/disabled/**")     // Excludes the specified paths from documentation
+                .build();
+    }
+}
