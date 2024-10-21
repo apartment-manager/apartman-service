4321936acfb29aa71fafe2ba5b3e66003bb51658
Khaled Asfur <khalid_asfur@hotmail.com>
2024-10-22 01:54:25 +0300

    Handle version exception

--- src/main/java/apartment/manager/Utilities/ExceptionToHttpStatusMapper.java
+++ src/main/java/apartment/manager/Utilities/ExceptionToHttpStatusMapper.java
@@ -2,6 +2,7 @@ package apartment.manager.Utilities;
 
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,7 @@ public class ExceptionToHttpStatusMapper {
     public ExceptionToHttpStatusMapper(){
         exceptionToStatusMap.put(NoSuchElementException.class, HttpStatus.NOT_FOUND);
         exceptionToStatusMap.put(ValidationException.class, HttpStatus.BAD_REQUEST);
+        exceptionToStatusMap.put(ObjectOptimisticLockingFailureException.class, HttpStatus.CONFLICT);
     }
     public HttpStatus getHttpStatus(Class<?extends Exception> exceptionClass){
         return exceptionToStatusMap.get(exceptionClass);
